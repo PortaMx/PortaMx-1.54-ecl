6b46f70219fee75cf5d830bdb043a2575bb9cf38
portamx <support@portamx.com>
2014-08-18 19:57:37 +0200

    small template fix

--- Themes/default/PortaMx/EclNoneModal.template.php
+++ Themes/default/PortaMx/EclNoneModal.template.php
@@ -5,7 +5,6 @@
 *
 * \author PortaMx - Portal Management Extension
 * \author Copyright 2008-2014 by PortaMx corp. - http://portamx.com
-* \author Copyright 2008-2014 by PortaMx corp. - http://portamx.com
 * \version 1.52
 * \date 18.08.2014
 */
